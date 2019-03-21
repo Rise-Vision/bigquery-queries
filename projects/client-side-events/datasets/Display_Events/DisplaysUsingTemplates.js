12f78c4ef31a90120b91c34a31acf1f1a8ade29d
santiagonoguez <santiago@nul-unu.com>
2019-03-21 11:04:48 -0600

    select displays using templates

---
+++ projects/client-side-events/datasets/Display_Events/DisplaysUsingTemplates.js
@@ -0,0 +1,29 @@
+#StandardSQL
+
+SELECT * FROM
+(
+  SELECT
+    DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) as date,
+    template,
+    display_id,
+    company_id
+  FROM
+  (
+    SELECT display_id, company_id, template.product_code AS template
+    FROM `client-side-events.Display_Events.events`
+    WHERE ts >= TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY))
+    AND ts < TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL -0 DAY))
+    AND platform = 'content'
+    AND display_id != 'DISPLAY_ID'
+    AND rollout_stage IN( 'beta', 'stable' )
+    AND template.product_code IS NOT NULL
+    GROUP BY 1, 2, 3
+  ) AS total
+
+  UNION ALL
+
+  SELECT date, template, display_id, company_id
+  FROM `client-side-events.Aggregate_Tables.DisplaysUsingTemplates`
+  WHERE date < DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY)
+)
+ORDER BY date DESC, template

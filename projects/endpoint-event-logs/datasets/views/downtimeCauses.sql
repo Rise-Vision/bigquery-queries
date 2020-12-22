e8e3fe39f34bcd874047c880583b27d4d1deb77e
alexey-rise <alexey.konovalov@risenext.ca>
2020-12-22 15:22:15 -0500

    added query that returns a list of errors that caused downtime

---
+++ projects/endpoint-event-logs/datasets/views/downtimeCauses.sql
@@ -0,0 +1,45 @@
+#standardSQL
+with
+
+endpointErrors as 
+(
+select 
+  date, 
+  endpointId, 
+  endpointType, 
+  companyId,
+  companyName,
+  companyIndustry,
+  parentCompanyId,
+  parentCompanyName,
+  networkCompanyId,
+  networkCompanyName,
+  networkCompanyIndustry,
+  eventErrorCode,
+  sum(errorCount) as errorCount
+from `endpoint-event-logs.logs.dailyErrors` E
+where  endpointType <> 'Unknown' and companyId is not null and (date between DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) and DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
+group by 1, 2, 3,4 ,5 ,6 ,7, 8, 9, 10, 11, 12
+)
+
+select distinct
+  date, 
+  endpointId, 
+  endpointType, 
+  companyId,
+  companyName,
+  companyIndustry,
+  parentCompanyId,
+  parentCompanyName,
+  networkCompanyId,
+  networkCompanyName,
+  networkCompanyIndustry,
+  eventErrorCode,
+  message,
+  description,
+  solution,
+  errorCount
+from endpointErrors E
+inner join `endpoint-event-logs.errors.errorDefinitions` D on E.eventErrorCode = D.code
+where isRelatedToUptime = 'Y'
+order by date desc, errorCount desc
